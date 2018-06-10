39a2829b1f3469149225263d12fb4b93c5650078
明天会更好 <373884384@qq.com>
2018-06-10 22:52:33 +0800

    代码重构

--- 油猴/Vip视频解析.js
+++ 油猴/Vip视频解析.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         一键VIP视频解析、去广告（全网）
 // @namespace    http://www.wandhi.com/
-// @version      1.4
+// @version      1.5
 // @description  在视频标题旁上显示“vip解析(去广告)”按钮和“搜索电影”按钮，在线播放vip视频；支持优酷vip，腾讯vip，爱奇艺vip，芒果vip，乐视vip等常用视频...
 // @author       Wandhi
 // @match        *://v.youku.com/v_show/*
@@ -44,9 +44,9 @@
     var reYJ = /1905/i;
     var rePP = /pptv/i;
     var reYYT = /yinyuetai/i;
-    var vipBtn = '<a id="wandhiVipBtn" style="cursor:pointer;text-decoration:none;color:red;padding:0 5px;border:1px solid red;">VIP解析</a>';
+    var vipBtn = '<a id="wandhiVipBtn" style="cursor:pointer;text-decoration:none;color:red;padding:0 5px;border:1px solid red;">vip解析</a>';
     var mSearchBtn = '<a id="wandhiSearchBtn" target="_blank" style="cursor:pointer;text-decoration:none;color:red;padding:0 5px;border:1px solid red;">搜索电影</a>';
-   
+
     // 爱奇艺
     if(reAqy.test(videoSite)){
         var iqiyiTitle = $('#widget-videotitle');
@@ -54,9 +54,9 @@
         $('#wandhiVipBtn').css({'font-size':'17px','display':'inline-block','height':'24px','line-height':'24px','margin':'0 5px'});
         $('#wandhiSearchBtn').css({'font-size':'17px','display':'inline-block','height':'24px','line-height':'24px','margin':'0 5px'});
         if($('#drama-series-title').length !== 0){
-        	curWords = $('#drama-series-title').find('a').text();
+            curWords = $('#drama-series-title').find('a').text();
         }else{
-        	curWords = iqiyiTitle.text();
+            curWords = iqiyiTitle.text();
         }
         $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords);
     }
@@ -67,10 +67,10 @@
         lsTitle.css('float','left');
         $('#wandhiVipBtn').css({'font-size':'16px','display':'inline-block','height':'20px','line-height':'20px','margin':'0 5px'});
         $('#wandhiSearchBtn').css({'font-size':'16px','display':'inline-block','height':'20px','line-height':'20px','margin':'0 5px'});
-       	if($('.Info').find('.title').find('h3').length !== 0){
-        	curWords = $('.Info').find('.title').find('h3').text();
+        if($('.Info').find('.title').find('h3').length !== 0){
+            curWords = $('.Info').find('.title').find('h3').text();
         }else{
-        	curWords = lsTitle.text();
+            curWords = lsTitle.text();
         }
         $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords);
     }
@@ -81,14 +81,14 @@
         $('#wandhiVipBtn').css({'font-size':'24px','display':'inline-block','height':'36px','line-height':'36px','margin':'0 5px'});
         $('#wandhiSearchBtn').css({'font-size':'24px','display':'inline-block','height':'36px','line-height':'36px','margin':'0 5px'});
         if($('.player_title').length !== 0 && $('.player_title').find('a').length === 0){
-        	curWords = $('.player_title').text();
+            curWords = $('.player_title').text();
         }else{
-        	curWords = $('._base_title').text();
+            curWords = $('._base_title').text();
         }
         if(curWords === ''){
-        	curWords = $('.player_title').text();
+            curWords = $('.player_title').text();
         }
-        $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords );
+        $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords);
     }
     // 土豆
     if(reTD.test(videoSite)){
@@ -103,8 +103,8 @@
         mgTitle.css({'float':'left','margin-right':'0'});
         $('#wandhiVipBtn').css({'font-size':'22px','display':'inline-block','height':'40px','line-height':'40px','margin':'0 5px'});
         $('#wandhiSearchBtn').css({'font-size':'22px','display':'inline-block','height':'40px','line-height':'40px','margin':'0 5px'});
-    	curWords = mgTitle.text();
-        $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords );
+        curWords = mgTitle.text();
+        $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords);
     }
     // 搜狐
     if(reSH.test(videoSite)){
@@ -136,40 +136,40 @@
         $('#wandhiVipBtn').css({'font-weight':'bold','font-size':'16px','display':'inline-block','height':'36px','line-height':'36px','margin':'0 5px'});
         $('#wandhiSearchBtn').css({'font-weight':'bold','font-size':'16px','display':'inline-block','height':'36px','line-height':'36px','margin':'0 5px'});
         curWords = pptvTitle.text();
-        $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords );
+        $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords);
     }
-	 if(reYk.test(videoSite)){
+    if(reYk.test(videoSite)){
         var youkuTitle = $('#subtitle');
         if(youkuTitle.length !== 0){
-        	youkuTitle.after(mSearchBtn).after(vipBtn);
-	        $('#wandhiVipBtn').css({'font-size':'17px','display':'inline-block','height':'22px','line-height':'22px','margin':'0 5px','vertical-align':'bottom'});
-	        $('#wandhiSearchBtn').css({'font-size':'17px','display':'inline-block','height':'22px','line-height':'22px','margin':'0 5px','vertical-align':'bottom'});
-	        if($('.tvinfo').length !== 0){
-	        	curWords = $('.tvinfo').find('h2').eq(0).find('a').text();
-	        }else{
-	        	curWords = $('.title').attr('title');
-	        }
-	        $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords);
+            youkuTitle.after(mSearchBtn).after(vipBtn);
+            $('#wandhiVipBtn').css({'font-size':'17px','display':'inline-block','height':'22px','line-height':'22px','margin':'0 5px','vertical-align':'bottom'});
+            $('#wandhiSearchBtn').css({'font-size':'17px','display':'inline-block','height':'22px','line-height':'22px','margin':'0 5px','vertical-align':'bottom'});
+            if($('.tvinfo').length !== 0){
+                curWords = $('.tvinfo').find('h2').eq(0).find('a').text();
+            }else{
+                curWords = $('.title').attr('title');
+            }
+            $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords);
         }else{
-        	$('.title').after(mSearchBtn).after(vipBtn);
-        	$('#wandhiVipBtn').css({'font-size':'17px','display':'inline-block','height':'22px','line-height':'22px','margin':'0 5px','vertical-align':'bottom'});
-	        $('#wandhiSearchBtn').css({'font-size':'17px','display':'inline-block','height':'22px','line-height':'22px','margin':'0 5px','vertical-align':'bottom'});
-	       	if($('.tvinfo').length !== 0){
-	        	curWords = $('.tvinfo').find('h3').eq(0).text();
-	        }else{
-	        	curWords = $('.title').attr('title');
-	        }
-	        $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords);
+            $('.title').after(mSearchBtn).after(vipBtn);
+            $('#wandhiVipBtn').css({'font-size':'17px','display':'inline-block','height':'22px','line-height':'22px','margin':'0 5px','vertical-align':'bottom'});
+            $('#wandhiSearchBtn').css({'font-size':'17px','display':'inline-block','height':'22px','line-height':'22px','margin':'0 5px','vertical-align':'bottom'});
+            if($('.tvinfo').length !== 0){
+                curWords = $('.tvinfo').find('h3').eq(0).text();
+            }else{
+                curWords = $('.title').attr('title');
+            }
+            $('#wandhiSearchBtn').attr('href','http://tv.wandhi.com/search/' + curWords);
         }
     }
     // 音悦台
     // if(reYYT.test(videoSite)){
-        // var yytTitle = $('.videoName');
-        // yytTitle.append(vipBtn);
-        // $('#wandhiVipBtn').css({'font-weight':'bold','font-size':'14px','display':'inline-block','height':'32px','line-height':'32px','margin':'0 5px'});
+    // var yytTitle = $('.videoName');
+    // yytTitle.append(vipBtn);
+    // $('#wandhiVipBtn').css({'font-weight':'bold','font-size':'14px','display':'inline-block','height':'32px','line-height':'32px','margin':'0 5px'});
     // }
-     $('#wandhiVipBtn').on('click',function(){
-         curPlaySite = window.location.href;
-         window.location.href = 'http://tv.wandhi.cn/go.html?url=' + curPlaySite;
-     });
+    $('#wandhiVipBtn').on('click',function(){
+        curPlaySite = window.location.href;
+        window.location.href = 'http://tv.wandhi.com/go.html?url=' + curPlaySite;
+    });
 })();
