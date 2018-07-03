7d02b7351f7c2377dc8d29d45905217298b960ca
明天会更好 <373884384@qq.com>
2018-07-03 10:11:44 +0800

    更新

--- 油猴/Vip视频解析.js
+++ 油猴/Vip视频解析.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         一键VIP视频解析、去广告（全网）
 // @namespace    http://www.wandhi.com/
-// @version      1.7
+// @version      1.8
 // @description  在视频标题旁上显示“vip解析(去广告)”按钮和“搜索电影”按钮，在线播放vip视频；支持优酷vip，腾讯vip，爱奇艺vip，芒果vip，乐视vip等常用视频...
 // @author       Wandhi
 // @match        *://v.youku.com/v_show/*
