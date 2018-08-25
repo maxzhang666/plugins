a8c383995474f79764380ab6462183b07bf9b9f8
明天会更好 <373884384@qq.com>
2018-08-25 11:47:13 +0800

    更新

--- 油猴/Vip视频解析.user.js
+++ 油猴/Vip视频解析.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
-// @name         一键VIP视频解析、去广告（全网），优惠券  2018-08-24 可用
+// @name         一键VIP视频解析、去广告（全网），优惠券  2018-08-25 可用
 // @namespace    http://www.wandhi.com/
-// @version      2.8.5
+// @version      2.8.6
 // @description  在视频播放页悬浮VIP按钮，可在线播放vip视频；支持优酷vip，腾讯vip，爱奇艺vip，芒果vip，乐视vip等常用视频...在淘宝天猫商品页添加优惠券查询按钮，可自行点击查询优惠券，
 // @author       Wandhi
+// @supportURL   https://shang.qq.com/wpa/qunwpa?idkey=9901cc46fa4626759852faec8a4f8b4b951a883e191670e60d3f940da0ef2749
 // @match        *://v.youku.com/v_show/*
 // @match        *://*.iqiyi.com/v_*
 // @match        *://*.iqiyi.com/w_*
