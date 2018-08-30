86ed7aeeff703b6469af6d28697568daa7e1b685
明天会更好 <373884384@qq.com>
2018-08-30 20:01:06 +0800

    音乐解析上线

--- 油猴/Vip视频解析.js
+++ 油猴/Vip视频解析.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         一键VIP视频解析、去广告（全网）,音乐解析下个版本更新 2018-08-29 可用
+// @name         一键VIP视频解析、去广告（全网）,一站式音乐搜索下载 2018-08-31 可用
 // @namespace    http://www.wandhi.com/
-// @version      2.8.7
-// @description  在视频播放页悬浮VIP按钮，可在线播放vip视频；支持优酷vip，腾讯vip，爱奇艺vip，芒果vip，乐视vip等常用视频...在淘宝天猫商品页添加优惠券查询按钮，可自行点击查询优惠券，
+// @version      2.9
+// @description  在视频播放页悬浮VIP按钮，可在线播放vip视频；支持优酷vip，腾讯vip，爱奇艺vip，芒果vip，乐视vip等常用视频...一站式音乐搜索解决方案，网易云音乐，QQ音乐，酷狗音乐，酷我音乐，虾米音乐，百度音乐，蜻蜓FM，荔枝FM，喜马拉雅...在淘宝天猫商品页添加优惠券查询按钮，可自行点击查询优惠券
 // @author       Wandhi
 // @match        *://v.youku.com/v_show/*
 // @match        *://*.iqiyi.com/v_*
@@ -34,6 +34,16 @@
 // @match        http*://item.taobao.com/*
 // @match        http*://detail.tmall.com/*
 // @match        http*://item.jd.com/*
+// @match        *://music.163.com/*
+// @match        *://y.qq.com/*
+// @match        *://www.kugou.com/*
+// @match        *://www.kuwo.cn/*
+// @match        *://www.xiami.com/*
+// @match        *://music.baidu.com/*
+// @match        *://www.qingting.fm/*
+// @match        *://www.lizhi.fm/*
+// @match        *://music.migu.cn/*
+// @match        *://www.ximalaya.com/*
 // @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
 // @require      https://cdn.bootcss.com/clipboard.js/1.5.16/clipboard.min.js
 // @grant        GM_setClipboard
@@ -47,7 +57,7 @@
     var reYk = /youku/i;
     var reAqy = /iqiyi/i;
     var reLS = /le.com/i;
-    var reTX = /qq/i;
+    var reTX = /v.qq/i;
     var reTD = /tudou/i;
     var reMG = /mgtv/i;
     var reSH = /sohu/i;
@@ -59,9 +69,53 @@
     var reTaoBao = /taobao/i;
     var reTmall = /tmall/i;
     var reJd = /jd/i;
+    var reWY = /163(.*)song/i;
+	var reQQ = /QQ(.*)song/i;
+	var reKG = /kugou(.*)song/i;
+	var reKW = /kuwo(.*)yinyue/i;
+	var reXM = /xiami/i;
+	var reBD = /baidu/i;
+	var reQT = /qingting/i;
+	var reLZ = /lizhi/i;
+	var reMG = /migu/i;
+	var reXMLY = /ximalaya/i;
     var html='';
     var name='';
-    if (reAqy.test(currentUrl) || reLS.test(currentUrl) || reTX.test(currentUrl) || reTD.test(currentUrl) || reMG.test(currentUrl) || reSH.test(currentUrl) || rePP.test(currentUrl) || reYk.test(currentUrl)) {
+    if(reWY.test(currentUrl)||reQQ.test(currentUrl)||reKG.test(currentUrl)||reKW.test(currentUrl)||reXM.test(currentUrl)||reBD.test(currentUrl)||reQT.test(currentUrl)||reLZ.test(currentUrl)||reMG.test(currentUrl)||reXMLY.test(currentUrl))
+    {
+        var sidenav = '<div class="aside-nav bounceInUp animated" id="aside-nav"><label for="" class="aside-menu" title="\u6309\u4f4f\u62d6\u52a8">VIP</label><a href="javascript:void(0)" title="\u7535\u5f71\u641c\u7d22" data-cat="search" class="menu-item menu-line menu-first">\u7535\u5f71<br>\u641c\u7d22</a><a href="javascript:void(0)" title="\u97f3\u4e50\u4e0b\u8f7d" data-cat="process" class="menu-item menu-line menu-second">\u97f3\u4e50<br>\u4e0b\u8f7d</a><a href="javascript:void(0)" title="\u7edd\u4e16\u597d\u5238" data-cat="tb" class="menu-item menu-line menu-third">\u7edd\u4e16<br>\u597d\u5238</a><a href="javascript:void(0)" title="\u4eac\u4e1c\u597d\u5238" data-cat="jd" class="menu-item menu-line menu-fourth">\u4eac\u4e1c<br>\u597d\u5238</a></div>';
+        $("body").append(sidenav).append($('<link rel="stylesheet" href="//tv.wandhi.com/static/style/asidenav.css">'));
+        var ua = navigator.userAgent;
+        /Safari|iPhone/i.test(ua) && 0 == /chrome/i.test(ua) && $("#aside-nav").addClass("no-filter");
+        var drags = { down: !1, x: 0, y: 0, winWid: 0, winHei: 0, clientX: 0, clientY: 0 }, asideNav = $("#aside-nav")[0], getCss = function (a, e) { return a.currentStyle ? a.currentStyle[e] : document.defaultView.getComputedStyle(a, !1)[e] };
+        $("#aside-nav").on("mousedown", function (a) {
+            drags.down = !0, drags.clientX = a.clientX, drags.clientY = a.clientY, drags.x = getCss(this, "right"), drags.y = getCss(this, "top"), drags.winHei = $(window).height(), drags.winWid = $(window).width(), $(document).on("mousemove", function (a) {
+                if (drags.winWid > 640 && (a.clientX < 120 || a.clientX > drags.winWid - 50))
+                    return !1;
+                if (a.clientY < 180 || a.clientY > drags.winHei - 120)
+                    return !1;
+                var e = a.clientX - drags.clientX,
+                    t = a.clientY - drags.clientY;
+                asideNav.style.top = parseInt(drags.y) + t + "px";
+                asideNav.style.right = parseInt(drags.x) - e + "px";
+            })
+        }).on("mouseup", function () {
+            drags.down = !1, $(document).off("mousemove")
+        });
+        $('body').on('click', '[data-cat=process]', function () {
+            window.open('http://music.wandhi.com/?url=' + currentUrl);
+        });
+        $('body').on('click', '[data-cat=search]', function () {
+            window.open('http://tv.wandhi.com/');
+        });
+        $('body').on('click', '[data-cat=tb]', function () {
+            window.open('http://www1.huizhek.com/');
+        });
+        $('body').on('click', '[data-cat=jd]', function () {
+            window.open('http://jd.huizhek.com');
+        });
+    } 
+    else if (reAqy.test(currentUrl) || reLS.test(currentUrl) || reTX.test(currentUrl) || reTD.test(currentUrl) || reMG.test(currentUrl) || reSH.test(currentUrl) || rePP.test(currentUrl) || reYk.test(currentUrl)) {
         var sidenav = '<div class="aside-nav bounceInUp animated" id="aside-nav"><label for="" class="aside-menu" title="\u6309\u4f4f\u62d6\u52a8">VIP</label><a href="javascript:void(0)" title="\u7535\u5f71\u641c\u7d22" data-cat="search" class="menu-item menu-line menu-first">\u7535\u5f71<br>\u641c\u7d22</a><a href="javascript:void(0)" title="\u89c6\u9891\u89e3\u6790" data-cat="process" class="menu-item menu-line menu-second">\u89c6\u9891<br>\u89e3\u6790</a><a href="javascript:void(0)" title="\u7edd\u4e16\u597d\u5238" data-cat="tb" class="menu-item menu-line menu-third">\u7edd\u4e16<br>\u597d\u5238</a><a href="javascript:void(0)" title="\u4eac\u4e1c\u597d\u5238" data-cat="jd" class="menu-item menu-line menu-fourth">\u4eac\u4e1c<br>\u597d\u5238</a></div>';
         $("body").append(sidenav).append($('<link rel="stylesheet" href="//tv.wandhi.com/static/style/asidenav.css">'));
         var ua = navigator.userAgent;
@@ -93,7 +147,8 @@
         $('body').on('click', '[data-cat=jd]', function () {
             window.open('http://jd.huizhek.com');
         });
-    } else if (reTaoBao.test(currentUrl)) {
+    }
+    else if (reTaoBao.test(currentUrl)) {
         name = $.trim($('.tb-main-title').text());
         html = '<div class="tb-btn-add" style="padding-top:10px;"><a target="_blank" href="http://www1.huizhek.com/index.php?r=searchlist&type=0&kwd=' + encodeURI(name) + '">\u9886\u53d6\u4f18\u60e0\u5238\u0028\u901a\u9053\u4e00\u0029</a></div>';
         html += '<div class="tb-btn-add" style="padding-top: 10px;padding-left: 10px;"><a target="_blank" href="http://www2.huizhek.com/index.php?r=l&kw=' + encodeURI(name) + '">\u9886\u53d6\u4f18\u60e0\u5238\u0028\u901a\u9053\u4e8c\u0029</a></div>';
