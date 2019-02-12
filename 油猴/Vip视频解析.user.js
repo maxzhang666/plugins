8f93df80ae3ca86514a526b88e4e008b8660eccf
明天会更好 <373884384@qq.com>
2019-02-12 22:40:37 +0800

    修正网易云音乐匹配

--- 油猴/Vip视频解析.user.js
+++ 油猴/Vip视频解析.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         一键VIP视频解析、去广告（全网）,一站式音乐搜索下载 2019-02-12 更新，报错请及时反馈
+// @name         一键VIP视频解析、去广告（全网）,一站式音乐搜索下载 2019-02-13 更新，报错请及时反馈
 // @namespace    http://www.wandhi.com/
 // @homepage     https://www.wandhi.com/post-647.html
 // @supportURL   https://www.wandhi.com/post-647.html
-// @version      3.1.5
+// @version      3.1.6
 // @description  在视频播放页悬浮VIP按钮，可在线播放vip视频；支持优酷vip，腾讯vip，爱奇艺vip，芒果vip，乐视vip等常用视频...一站式音乐搜索解决方案，网易云音乐，QQ音乐，酷狗音乐，酷我音乐，虾米音乐，百度音乐，蜻蜓FM，荔枝FM，喜马拉雅，优惠券查询
 // @author       Wandhi
 // @icon         https://www.wandhi.com/favicon.ico
@@ -118,7 +118,7 @@
 
             }else
             {
-                window.open('http://music.wandhi.com/?url=' + currentUrl);
+                window.open('http://music.wandhi.com/?url=' + encodeURIComponent(currentUrl));
             }
         });
         $('body').on('click', '[data-cat=search]', function () {
