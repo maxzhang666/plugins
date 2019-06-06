55d20f6964c0eea994918620759968c1c7cdbd01
明天就更好 <373884384@qq.com>
2019-06-06 21:06:59 +0800

    版本推送

--- 油猴/Vip视频解析.js
+++ 油猴/Vip视频解析.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         一键VIP视频解析、去广告（全网）,一站式音乐搜索下载 2019-06-06 更新，报错请及时反馈
 // @namespace    http://www.wandhi.com/
-// @version      3.3.0
+// @version      3.3.1
 // @description  在视频播放页悬浮VIP按钮，可在线播放vip视频；支持优酷vip，腾讯vip，爱奇艺vip，芒果vip，乐视vip等常用视频...一站式音乐搜索解决方案，网易云音乐，QQ音乐，酷狗音乐，酷我音乐，虾米音乐，百度音乐，蜻蜓FM，荔枝FM，喜马拉雅，优惠券查询
 // @author       Wandhi
 // @icon         https://www.wandhi.com/favicon.ico
